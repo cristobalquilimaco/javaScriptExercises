dfcbb45d1f588bfd16634805825fac079266d233
Cristobal <quilimacox1@gmail.com>
2024-12-08 06:57:41 -0500

    Se termina descripcion del ejercicio

--- loops.js
+++ loops.js
@@ -1,2 +1,15 @@
-// SUMA DE MULTIPLOS DE 3 Y 5 
+// // SUMA DE MULTIPLOS DE 3 Y 5 
+
+// Escribe un programa que calcule la suma de todos los numeros que tengan multiplos de 3 o 5 que sean menores a 1000
+
+// instrucciones:
+// 1 ) crea un bucle que recorra todos los numeros del 1 hasta el 9999
+// 2 ) Si el numero es multiplo de 3 o 5. añadelo a una variable suma 
+// 3 ) Al final del programa, muestra el resultado de la suma en la consola
+
+// PISTAS 
+// Puedes utilizar el operador % para verificar si el numero es multiplo de otro Por ejempo, n % 3 === 0 significa que n es un multiplo de 3.
+// Usa el bucle for para recorrer los numeros.
+
+// Ejemplo: Suma de los multiplos de 3 y 5 menores a 1000 : 
 
