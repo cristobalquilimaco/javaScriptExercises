8633fc164d8def3eef8f4df11b4bd65964b211ce
Cristobal <quilimacox1@gmail.com>
2024-12-08 05:13:42 -0500

    Se agregan indicaciones del ejercicio

--- basic1.js
+++ basic1.js
@@ -26,5 +26,9 @@ console.log(`el valor total de tu compra es de ${totalCompra}€`);
 // En una variable tienes el lado de un cuadrado, debes escribir un programa que te calcule el área y el perímetro del cuadrado.
 // El area la calculas como lado multiplicado por lado. El perímetro es la suma de los cuatro lados.
 
+// INDICACIONES:
 
+// LADO = 40
+// AREA = 1600
+// PERIMETRO 160
 
