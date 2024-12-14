f49fbbce93f66c7ebbfe2ff4e4285bfdb25a8650
Cristobal <quilimacox1@gmail.com>
2024-12-14 05:06:59 -0500

    Se finaliza documentacion del ejercicio

--- orderWords.js
+++ orderWords.js
@@ -1,3 +1,6 @@
-Ordenar una lista de palabras alfabéticamente
+// Ordenar una lista de palabras alfabéticamente
 
-Instrucciones:
+// Instrucciones:
+// 1 - Crea una funcion llamada "orderWords" que reciba un arreglo de palabras y las ordene alfabéticamente de forma ascendente.
+// 2 - Crea otra función llamada "showResults" que imprima el arreglo ordenado en consola
+// 3 - La funcion ordenar "orderWords" debe ordenar las palabras usando algún algoritmo de ordenamiento (por ejemplo el metodo sort de javascript)
