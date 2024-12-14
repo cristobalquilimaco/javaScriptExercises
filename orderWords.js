dd3eb8fdba144564cb0d5f4fbf3313128557d614
Cristobal <quilimacox1@gmail.com>
2024-12-14 05:08:22 -0500

    Se agrega funcion orderWords

--- orderWords.js
+++ orderWords.js
@@ -4,3 +4,8 @@
 // 1 - Crea una funcion llamada "orderWords" que reciba un arreglo de palabras y las ordene alfabéticamente de forma ascendente.
 // 2 - Crea otra función llamada "showResults" que imprima el arreglo ordenado en consola
 // 3 - La funcion ordenar "orderWords" debe ordenar las palabras usando algún algoritmo de ordenamiento (por ejemplo el metodo sort de javascript)
+
+function orderWords(word){
+    return word.sort();
+}
+
