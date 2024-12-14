2ee2c17eccc1e711d03134f0a676686ab7155e77
Cristobal <quilimacox1@gmail.com>
2024-12-13 20:54:43 -0500

    Se finaliza for

--- numeroPrimo.js
+++ numeroPrimo.js
@@ -16,6 +16,9 @@ function primeNumber(){
     }
 
     for(let i = 2; i <= Math.sqrt(num); i++){
-        if(num)
+        if(num % i === 0){
+            return false;
+        }
     }
+    return true
 }
