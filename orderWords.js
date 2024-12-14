a5e3434ced490390e1653152507b561d4457cb22
Cristobal <quilimacox1@gmail.com>
2024-12-14 05:20:27 -0500

    Se documenta cada funcion

--- orderWords.js
+++ orderWords.js
@@ -5,14 +5,18 @@
 // 2 - Crea otra función llamada "showResults" que imprima el arreglo ordenado en consola
 // 3 - La funcion ordenar "orderWords" debe ordenar las palabras usando algún algoritmo de ordenamiento (por ejemplo el metodo sort de javascript)
 
+//Se crea la funcion para ordenar alfabéticamente las palabras
 function orderWords(words){
     return words.sort();
 }
 
+//Funcion para mostrar los resultados de las palabras
 function showResults(words){
     const orderWordsFinal = orderWords(words)
     console.log("Palabras ordenadas alfabéticamente: ", orderWordsFinal);
 }
 
+//Seccion para probar el ejrcicio
 const wordList = ["manzana", "naranja", "pera", "platano", "uva"]
 showResults(wordList)
+
