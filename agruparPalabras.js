551b9b9f0dd4381026f21c8805c5cd5221a73ce4
Cristobal <quilimacox1@gmail.com>
2024-12-16 05:23:15 -0500

    Se documenta ejercicio

---
+++ agruparPalabras.js
@@ -0,0 +1,12 @@
+Funcion agruparPoLongitud(palabras)
+
+Recibe un array de palabras como parametro 
+
+Devuelve un objeto donde las claves sean longitudes de las palabras y los valores 
+sean arrays con las palabras que tienen esa longitud
+
+Validaciones:
+
+Asegurate de que el array no este vacio
+Si esta vacio la funcion debe retornar un array vacio ({})
+
