41b41daaa7e3dbddf5d4f267deccd972a5f219c5
Cristobal <quilimacox1@gmail.com>
2024-12-17 19:47:47 -0500

    Se inicia practica de ejercicio fizzBuzz

---
+++ fizzBuzz.js
@@ -0,0 +1,11 @@
+function fizzBuzz(){
+    let resultado = []
+
+    for(let i = 1; i <= n; i++){
+        let resultadoFizzBuzz = "";
+        if( i % 3 === 0){
+            return "Fizz"
+        }
+        if(i % 5)   
+    }
+}
