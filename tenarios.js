db54a5605591050e809bddb10c193283137eb74b
Cristobal <quilimacox1@gmail.com>
2025-01-19 08:34:31 -0500

    Se agrega documentacion

--- tenarios.js
+++ tenarios.js
@@ -6,3 +6,7 @@ if(patas === 4 && accion === 'ladra'){
 }else{
     console.log('No es un perro')
 }
+
+// Los operadores ternarios en este ejercicion consta de 3 partes aqui no es necesario el if ya que los operadores lo reemplazan
+// Parte 1 la condicion () --- Parte 2 ? ---- parte 3  :  
+
