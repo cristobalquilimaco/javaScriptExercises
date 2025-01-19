35e5e80cf36fe434721308d368a362211943afb8
Cristobal <quilimacox1@gmail.com>
2025-01-19 08:37:02 -0500

    Se finaliza condicion

--- tenarios.js
+++ tenarios.js
@@ -1,13 +1,13 @@
 let patas = 4
 let accion = 'ladra'
 
-if(patas === 4 && accion === 'ladra'){
-    console.log("Es un perro y ladra");
-}else{
-    console.log('No es un perro')
-}
+// if(patas === 4 && accion === 'ladra'){
+//     console.log("Es un perro y ladra");
+// }else{
+//     console.log('No es un perro')
+// }
 
 // Los operadores ternarios en este ejercicion consta de 3 partes aqui no es necesario el if ya que los operadores lo reemplazan
 // Parte 1 la condicion () --- Parte 2 ? ---- parte 3  :  
 
-(patas === 4) && (accion === 'ladrar')
+(patas === 4) && (accion === 'ladrar') ? console.log("Es un perro y ladra") : console.log('No es un perro');
