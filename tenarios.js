eff72fb75beacf7bbb78a97f72aac08985e93f4c
Cristobal <quilimacox1@gmail.com>
2025-01-19 08:40:16 -0500

    Se ajusta ejercicio con ;

--- tenarios.js
+++ tenarios.js
@@ -1,5 +1,5 @@
-let patas = 4
-let accion = 'ladra'
+let patas = 4;
+let accion = 'ladra';
 
 // if(patas === 4 && accion === 'ladra'){
 //     console.log("Es un perro y ladra");
@@ -10,4 +10,4 @@ let accion = 'ladra'
 // Los operadores ternarios en este ejercicion consta de 3 partes aqui no es necesario el if ya que los operadores lo reemplazan
 // Parte 1 la condicion () --- Parte 2 ? ---- parte 3  :  
 
-(patas === 4) && (accion === 'ladrar') ? console.log("Es un perro y ladra") : console.log('No es un perro');
+(patas == 4) && (accion == 'ladra') ? console.log("Es un perro y ladra") : console.log('No es un perro');
