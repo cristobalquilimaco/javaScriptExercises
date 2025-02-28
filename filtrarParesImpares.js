e92534c4e7050279c14a38673301a45ba340049d
Cristobal <quilimacox1@gmail.com>
2025-02-28 07:07:16 -0500

    Se crea funcion para filtrar pares impares

--- filtrarParesImpares.js
+++ filtrarParesImpares.js
@@ -1,9 +1,11 @@
 // Ejercicio: Filtrar pares e impares
-// Tienes un array dEjercicio: Filtrar pares e impares
 // Tienes un array de números enteros. Tu tarea es escribir una función llamada filtrarParesImpares que reciba este array y devuelva un objeto con dos propiedades:
 
 // pares: un array con todos los números pares.
-// impares: un array con todos los números impares.e números enteros. Tu tarea es escribir una función llamada filtrarParesImpares que reciba este array y devuelva un objeto con dos propiedades:
-
-// pares: un array con todos los números pares.
 // impares: un array con todos los números impares.
+
+function filtrarParesImpares(){
+
+}
+
+numbers = [1,2,3,4,5,6,7,8,9]
