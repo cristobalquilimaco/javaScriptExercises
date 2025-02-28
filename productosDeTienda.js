296b0d2dd57962ca9e3a7394dbeb7ac9caadfda9
Cristobal <quilimacox1@gmail.com>
2025-02-27 19:20:56 -0500

    Se crea ejercicio para calcular el valor total de los productos

---
+++ productosDeTienda.js
@@ -0,0 +1,16 @@
+// Ejercicio:
+// Tienes un array de objetos que representan productos en una tienda. Cada objeto 
+// tiene las siguientes propiedades: nombre, precio y cantidad. La tarea es escribir una función que 
+// calcule el valor total de los productos en inventario, teniendo en cuenta la cantidad de cada uno.
+
+// Requerimientos:
+// Crea una función llamada calcularTotalInventario.
+// La función debe aceptar un array de objetos, donde cada objeto representa un producto.
+// La función debe devolver el valor total de todos los productos en inventario.
+
+const productos = [
+    { nombre: 'Producto A', precio: 30, cantidad: 5 },
+    { nombre: 'Producto B', precio: 20, cantidad: 3 },
+    { nombre: 'Producto C', precio: 10, cantidad: 7 }
+  ];
+  
