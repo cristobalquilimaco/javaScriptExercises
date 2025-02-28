19aad511b9cd3800953da8085c1d36b1ca9db283
Cristobal <quilimacox1@gmail.com>
2025-02-28 07:05:19 -0500

    Se inicia ejercicio de logica nivel basico

---
+++ filtrarParesImpares.js
@@ -0,0 +1,9 @@
+// Ejercicio: Filtrar pares e impares
+// Tienes un array dEjercicio: Filtrar pares e impares
+// Tienes un array de números enteros. Tu tarea es escribir una función llamada filtrarParesImpares que reciba este array y devuelva un objeto con dos propiedades:
+
+// pares: un array con todos los números pares.
+// impares: un array con todos los números impares.e números enteros. Tu tarea es escribir una función llamada filtrarParesImpares que reciba este array y devuelva un objeto con dos propiedades:
+
+// pares: un array con todos los números pares.
+// impares: un array con todos los números impares.
