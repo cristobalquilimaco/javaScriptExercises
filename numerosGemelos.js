08ea4ef8db18c7d53dc22d1965fbde0b7d5ed94c
Cristobal <quilimacox1@gmail.com>
2025-03-01 17:00:40 -0500

    Se crea nuevo ejercicio de numero primos gemelos

---
+++ numerosGemelos.js
@@ -0,0 +1,11 @@
+// Ejercicio: Encuentra los números primos en un rango dado y verifica si son primos gemelos
+// Descripción del problema:
+// Rango de números: Dado un número n, se te pide que encuentres todos los números primos entre 2 y n.
+// Primos gemelos: Un par de números primos (p1, p2) son considerados primos gemelos si la diferencia entre ellos es exactamente 2. Es decir, si p2 - p1 === 2 y ambos son números primos.
+// Requisitos:
+// Crea una función que encuentre todos los números primos entre 2 y un número n dado.
+// Luego, encuentra y devuelve todos los pares de primos gemelos en ese rango.
+// La función debe devolver un array con todos los pares de primos gemelos encontrados.
+// La salida debe ser un array de arrays, donde cada subarray contiene un par de primos gemelos.
+
+
