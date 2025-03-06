fd195396d515a4e206995b14fe01f79083c80298
Cristobal <quilimacox1@gmail.com>
2025-03-06 17:53:58 -0500

    Se crea ejercicio encontrar el numero unico

---
+++ numeroUnico.js
@@ -0,0 +1,7 @@
+// Ejercicio: Encontrar el número único
+// Tienes un arreglo de números donde todos los números aparecen dos veces, 
+// excepto uno que aparece una sola vez. Tu tarea es encontrar el número único que aparece solo una vez.
+
+
+
+[4, 1, 2, 1, 2]
